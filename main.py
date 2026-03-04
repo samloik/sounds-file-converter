1b5b22b0a0c5af119a1c7160540d78521b6aa8b2
Alex Loik <samloik@mail.ru>
2026-03-04 18:19:40 +1000

    rewrite instruction

--- main.py
+++ main.py
@@ -208,4 +208,4 @@ async def health_check():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True, log_level="info")
